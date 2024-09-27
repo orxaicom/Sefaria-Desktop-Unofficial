2aae56ca3b13e2e740f9d01d0c5bbe58ec224244
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-27 09:49:20 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+set -e
+
+# Install dependencies
+apt-get update
+apt-get -y upgrade
+apt-get -y install curl unzip
+
+# Install python requirements
 curl -OL "https://github.com/Sefaria/Sefaria-Project/archive/refs/heads/master.zip"
 unzip master.zip > /dev/null && rm master.zip
 mv Sefaria-Project-master Sefaria-Project
@@ -12,10 +20,11 @@ mv Sefaria-Container-Unofficial-main/local_settings.py sefaria && rm -rf Sefaria
 mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
 /python3.8 manage.py migrate
 
-# npm
+# Install npm modules
 npm install
 npm run setup
 npm run build
 npm run build-client
 
+# Debugging
 ls -lrtha
