f3b71cda2ebf22a9bf5d95cc8468ec51d4bbe487
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-27 19:46:16 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 # Install dependencies
 apt-get update
 apt-get -y upgrade
-apt-get -y install curl unzip fuse
+apt-get -y install curl unzip fuse file
 
 # Migrate
 curl -OL "https://github.com/Sefaria/Sefaria-Project/archive/refs/heads/master.zip"
