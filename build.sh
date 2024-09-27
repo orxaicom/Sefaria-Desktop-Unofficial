f7170e5fa9856e17425be1748cd7bcd659cd8bc3
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-27 17:07:06 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,14 +28,14 @@ npm run build-client
 # Build the AppImage directory
 cd /
 mkdir -p MyApp.AppDir/{usr/bin,usr/lib,data/db,var/log,workspaces}
-cp /usr/bin/mongod MyApp.AppDir/usr/bin/
-cp /usr/bin/redis-server MyApp.AppDir/usr/bin/
-cp -r /python3.8.13-cp38-cp38-manylinux2010_x86_64.AppDir MyApp.AppDir/
-cp -r /workspaces/Sefaria-Project MyApp.AppDir/workspaces/
-cp /workspaces/assets/AppRun MyApp.AppDir/
+cp /usr/bin/mongod MyApp.AppDir/usr/bin
+cp /usr/bin/redis-server MyApp.AppDir/usr/bin
+cp -r /python3.8.13-cp38-cp38-manylinux2010_x86_64.AppDir MyApp.AppDir
+cp -r /workspaces/Sefaria-Project MyApp.AppDir/workspaces
+cp /workspaces/assets/AppRun MyApp.AppDir
 chmod +x MyApp.AppDir/AppRun
-cp /workspaces/assets/MyApp.desktop MyApp.AppDir/
-cp /workspaces/assets/myapp.png MyApp.AppDir/
+cp /workspaces/assets/MyApp.desktop MyApp.AppDir
+cp /workspaces/assets/myapp.png MyApp.AppDir
 
 # Build the AppImage
 ls -lrtha
