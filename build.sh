5bfa18e75a562329828abd74356933cddabc43b6
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-29 10:51:51 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,9 +13,7 @@ unzip master.zip > /dev/null && rm master.zip
 mv Sefaria-Project-master Sefaria-Project
 cd Sefaria-Project
 mkdir log && chmod 777 log
-curl -OL "https://github.com/orxaicom/Sefaria-Container-Unofficial/archive/refs/heads/main.zip"
-unzip main.zip >/dev/null && rm main.zip
-mv Sefaria-Container-Unofficial-main/local_settings.py sefaria && rm -rf Sefaria-Container-Unofficial-main
+cp /workspaces/assets/local_settings.py sefaria
 mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
 /python3.8 manage.py migrate
 
@@ -33,10 +31,21 @@ mkdir -p MyApp.AppDir/data
 mkdir -p MyApp.AppDir/var/log
 mkdir -p MyApp.AppDir/workspaces
 
+# Mongo
 cp /usr/bin/mongod MyApp.AppDir/usr/bin
 ldd /usr/bin/mongod | grep "=> /" | awk '{print $3}' | xargs -I {} cp {} MyApp.AppDir/usr/lib/
+
+# Redis
 cp /usr/bin/redis-server MyApp.AppDir/usr/bin
 ldd /usr/bin/redis-server | grep "=> /" | awk '{print $3}' | xargs -I {} cp {} MyApp.AppDir/usr/lib/
+
+# gettext
+cp /usr/bin/gettext MyApp.AppDir/usr/bin
+ldd /usr/bin/gettext | grep "=> /" | awk '{print $3}' | xargs -I {} cp {} MyApp.AppDir/usr/lib/
+cp -r /usr/lib/x86_64-linux-gnu/gettext MyApp.AppDir/usr/lib/x86_64-linux-gnu
+cp /usr/lib/x86_64-linux-gnu/libgettextlib-0.21.so MyApp.AppDir/usr/lib/x86_64-linux-gnu
+cp /usr/lib/x86_64-linux-gnu/libgettextsrc-0.21.so MyApp.AppDir/usr/lib/x86_64-linux-gnu
+
 cp -r /python3.8.13-cp38-cp38-manylinux2010_x86_64.AppDir MyApp.AppDir
 cp -r /workspaces/Sefaria-Project MyApp.AppDir/workspaces
 cp /workspaces/assets/AppRun MyApp.AppDir
@@ -50,12 +59,7 @@ mkdir MyApp.AppDir/usr/lib/x86_64-linux-gnu
 cp /usr/lib/x86_64-linux-gnu/libatomic.so.1 MyApp.AppDir/usr/lib/x86_64-linux-gnu
 
 # Build the AppImage
-ls -lrtha
 curl -OL "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
 ARCH=x86_64 ./appimagetool-x86_64.AppImage --appimage-extract-and-run MyApp.AppDir && rm -rf MyApp.AppDir
-ls -lrtha
-echo "============================================================================="
 mv MyApp-x86_64.AppImage /workspaces
-ls -lrtha /workspaces
-pwd
