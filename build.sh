fe3566fb2b1f1dabbea04857da0cf9b7c57e2a1e
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-29 11:32:29 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ unzip master.zip > /dev/null && rm master.zip
 mv Sefaria-Project-master Sefaria-Project
 cd Sefaria-Project
 mkdir log && chmod 777 log
-cp /workspaces/assets/local_settings.py sefaria
+curl -OL "https://github.com/orxaicom/Sefaria-Container-Unofficial/archive/refs/heads/main.zip"
+unzip main.zip >/dev/null && rm main.zip
+mv Sefaria-Container-Unofficial-main/local_settings.py sefaria && rm -rf Sefaria-Container-Unofficial-main
 mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
 /python3.8 manage.py migrate
 
@@ -47,6 +49,7 @@ cp /usr/lib/x86_64-linux-gnu/libgettextlib-0.21.so MyApp.AppDir/usr/lib/x86_64-l
 cp /usr/lib/x86_64-linux-gnu/libgettextsrc-0.21.so MyApp.AppDir/usr/lib/x86_64-linux-gnu
 
 cp -r /python3.8.13-cp38-cp38-manylinux2010_x86_64.AppDir MyApp.AppDir
+sed -i "s;'NAME': '/workspaces;'NAME': './workspaces;" /workspaces/Sefaria-Project/sefaria/local_settings.py
 cp -r /workspaces/Sefaria-Project MyApp.AppDir/workspaces
 cp /workspaces/assets/AppRun MyApp.AppDir
 chmod +x MyApp.AppDir/AppRun
