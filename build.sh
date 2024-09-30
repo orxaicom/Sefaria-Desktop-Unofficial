b940312175e8a5d85536561a2bddbce41ce0488b
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-30 21:48:37 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ cp /usr/lib/x86_64-linux-gnu/libatomic.so.1 MyApp.AppDir/usr/lib/x86_64-linux-gn
 
 # Final migrate
 cd MyApp.AppDir
-pkill -9 mongod && ./usr/bin/mongod --fork --logpath ./var/log/mongodb.log --dbpath ./data/db
+pkill -9 mongod && ./usr/bin/mongod --logpath ./var/log/mongodb.log --dbpath ./data/db
 ./python3.8.13-cp38-cp38-manylinux2010_x86_64.AppDir/opt/python3.8/bin/python3.8 ./workspaces/Sefaria-Project/manage.py migrate
 cd /
 
