83fec3694b182bfc5f65cfaa05b41a424c1055fd
ORX AI <131006215+orxai@users.noreply.github.com>
2024-10-02 17:28:02 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -77,11 +77,6 @@ cd /
 #mv MyApp-x86_64.AppImage /workspaces
 
 # Build Electron
-# To fix --no-sandbox issue
-whoami
-useradd -m myuser
-su myuser
-whoami
 mkdir sefaria-electron
 cd sefaria-electron
 npm init -y
