1e48696598eec4fd0cee488cb692d4403634947d
ORX AI <131006215+orxai@users.noreply.github.com>
2024-10-03 07:01:45 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install curl unzip fuse file appstream gnupg procps \
                    libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 \
-                   libgtk-3-0 libasound2
+                   libgtk-3-0 libasound2 zip
 
 # Migrate
 curl -OL "https://github.com/Sefaria/Sefaria-Project/archive/refs/heads/master.zip"
